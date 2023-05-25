39a7b5cf5ab29b066ab02faa9245cb7ffd5e5c89
emagici <skrullweb.dev@gmail.com>
2023-05-24 21:35:02 -0400

    change format

--- src/components/landing/HeroSection/index.js
+++ src/components/landing/HeroSection/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 
 export function HeroSection() {
   return (
-    <div className="animate-wiggle grow bg-no-repeat bg-cover relative w-full h-full flex justify-center bg-animation-back">
+    <div className="bg-animation-back animate-wiggle grow bg-no-repeat bg-cover relative w-full h-full flex justify-center">
       <div className="flex flex-col mix-blend-multiply w-[673px] h-[354px] items-center bg-[#222C2E] shadow-sm bg-opacity-90 absolute bottom-0 shadow-[#076A80] border border-[#778A8F] py-7 px-[107px]"></div>
       <div className="flex flex-col items-center absolute bottom-4 gap-4">
         <h1 className="text-[32px] text-center text-shadow shadow-[#BD3A02] w-[450px]">
